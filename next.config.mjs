dc864e8a262301b995b1fbdd723364dce932c26a
Madiso Melese <madishamadiso00@gmail.com>
2024-12-24 15:32:42 +0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: { OPENAI_API_KEY: process.env.OPENAI_API_KEY
+},
+}
+export default nextConfig;
---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/components/Translator.tsx', // Your entry file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/')
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // Add more extensions if needed
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
